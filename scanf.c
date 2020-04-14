5881d9f7615ac4648947a07bad9aa5a31192dfa3
Ashraf <sm.ashraf.zaman>
2020-04-14 16:18:29 +0600

    Create scanf.c
    initial commit

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter your int number : ");
+    scanf("%d", &num);
+    printf("Your int number is : %d", num);
+    getch(); //remove extra lines
+    return 0;
+}
